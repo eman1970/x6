2b53becdd8f7150c41fdcba27d105920825aebb8
eman1970 <tommy.hallqvist@gmail.com>
2021-02-22 17:05:36 +0100

    Thrird commit

--- server/Server.js
+++ server/Server.js
@@ -1,9 +1,20 @@
-
 import express from 'express'
 
 const app = express()
 
 app.listen(3001, () => {
-    console.log("Servern är igång på port 3001")
+    console.log('Servern är igång på port' +3001)
+
+
+})
+
+
+app.get('/recipe', (request, response) => {
+    response.send('Ditt API-anrop gick igenom!')
+
+})
+
+app.get('/throwdice', (request, response) => {
+    response.send(Math.random().toString())
 
 })
