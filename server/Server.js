fefa2c266099506a2062ee0edf39d27cc5350cc2
eman1970 <tommy.hallqvist@gmail.com>
2021-02-23 16:52:19 +0100

    Fifth commit

--- server/Server.js
+++ server/Server.js
@@ -1,9 +1,21 @@
 import express from 'express'
+import helmet from 'helmet'
+import mongoose from 'mongoose'
+import morgan from 'morgan'
 
 const app = express()
+app.use(helmet())
+app.use(morgan('common'))
+
+const checkIfAdmin = (request, response, next) => {
+    console.log(request.query.username)
+    next()
+}
+
+app.use(checkIfAdmin)
 
 app.listen(3001, () => {
-    console.log('Servern är igång på port' +3001)
+    console.log('Servern är igång på port' + 3001)
 
 
 })
@@ -14,7 +26,14 @@ app.get('/recipe', (request, response) => {
 
 })
 
-app.get('/throwdice', (request, response) => {
+app.get('/throwdice', checkIfAdmin, (request, response) => {
     response.send(Math.random().toString())
 
 })
+
+mongoose.connect('mongodb://localhost/namndb', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Successfully connected to the database'))
+    .catch((error) => {
+        console.log('ERROR WHILE TRYING TO CONNECT TO THE DATABASE: ' + error)
+        process.exit()
+    })
