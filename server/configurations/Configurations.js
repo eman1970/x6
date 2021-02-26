f77825722b4825d3afe8c1d9adfd0b3663991e68
eman1970 <tommy.hallqvist@gmail.com>
2021-02-26 22:55:21 +0100

    Housekeeping 2

--- server/configurations/Configurations.js
+++ server/configurations/Configurations.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 
 dotenv.config()
-const { DATABASE_URL, PORT} = process.env
+const { DATABASE_URL, PORT } = process.env
 
 const connectToDatabase = async () => {
     try {
@@ -23,9 +23,20 @@ const connectToPort = (app) => {
 }
 
 
- 
+
 
 export default {
     connectToDatabase,
     connectToPort
 }
+
+/* mongoose.connect('mongodb://localhost/namndb', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+    .then(() console.log('SUCCESSFULLY CONNECTED TO THE DATABASE')).catch((error) => {
+        console.log('ERROR WHILE TRYING TO CONNECT TO THE DATABASE: ' + error)
+        process.exit()
+
+    })
+
+app.listen(300, () => {
+    console.log('Servern är igång på port ' + 3001)
+}) */
